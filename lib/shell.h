5f4307f600a236f38efde454560bc57f6f25cd97
AlinaKhirullina <alinakhairullina200407@gmail.com>
2024-02-09 00:45:39 +0300

    add lib.shell.h

--- lib/shell.h
+++ lib/shell.h
@@ -2,6 +2,7 @@
 #define sep ";"
 #define space " "
 #define conv "|"
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <stdlib.h>
@@ -10,12 +11,21 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <signal.h>
-char * read_line();
+
+char* read_line();
+
 void split_line1(char* line);
+
 void split_line2(char* token);
-char * brack(char* s);
+
+char* brack(char* s);
+
 int b_cd (char* arg);
+
 int token_is_conv(char* s);
+
 int execute_cmd(char* s);
+
 int conveer(char* s);
+
 int b_cd (char* arg);
